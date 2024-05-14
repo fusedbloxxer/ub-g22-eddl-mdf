81f55af947122750166399a731d7755e99c2188f
fusedbloxxer <voidwarden@yahoo.com>
2024-05-14 22:18:00 +0300

    Create mesh sampler

--- mdf/src/mdf/ops.py
+++ mdf/src/mdf/ops.py
@@ -1,4 +1,5 @@
 import typing as t
+from typing import overload
 import torch
 import torch.nn as nn
 from torch import Tensor
@@ -6,55 +7,94 @@ import numpy as np
 import numpy.typing as npt
 from pyvista import PolyData
 from collections import namedtuple
+from torch import multinomial
+from torch.distributions import Dirichlet
 import einops
 
 
-class LBOEmbedding:
+class MeshSampler:
+    """Sample points on a 3d mesh uniformly."""
+
     def __init__(
         self,
         verts: Tensor,
         faces: Tensor,
-        k_evecs: int,
+        device:torch.device = torch.device('cpu'),
+    ) -> None:
+        super(MeshSampler, self).__init__()
+        self.verts: Tensor = verts
+        self.faces: Tensor = faces
+        self.device: torch.device = device
+        self.dirichlet = Dirichlet(concentration=torch.ones(3))
+
+    def sample(self, n: int, indices: bool=True) -> t.Tuple[Tensor, Tensor, Tensor]:
+        # Sample n triangle mesh faces without replacement
+        sample_verts_idx: Tensor = multinomial(torch.ones(self.faces.size(0)), num_samples=n, replacement=False)
+        sample_faces: Tensor = self.faces[sample_verts_idx]
+
+        # Sample barycentric coordinates for each triangle mesh
+        coefs: Tensor = self.dirichlet.sample(torch.Size([n])).type(torch.float32)
+
+        # Compute R^3 coordinates based on barycentric interpolation
+        pos: Tensor = self.verts.gather(dim=0, index=einops.repeat(sample_faces[:, 1:], 'N T -> (N T) C', C=self.verts.size(1)))
+        pos = einops.rearrange(pos, '(N T) C -> N T C', N=sample_faces.size(0)).type(torch.float32)
+        pos = torch.einsum('NT,NTC->NC', coefs, pos)
+
+        sample_faces = sample_faces if indices is False else self.faces[sample_verts_idx][:, 1:]
+        return pos, sample_faces, coefs
+
+    @staticmethod
+    def from_poly(poly: PolyData, *args, **kwargs) -> 'MeshSampler':
+        verts: Tensor = torch.tensor(poly.points)
+        faces: Tensor = torch.tensor(poly.faces.reshape((-1, 4)))[:, 1:]
+        return MeshSampler(verts, faces, *args, **kwargs)
+
+
+class LBOEmbedding:
+    """
+    Compute the first k eigenvectors accordin to the smallest eigenvalues for a 3d mesh using
+    eigen-decomposition of LBO (symmetric normalized graph Laplacian).
+    """
+
+    def __init__(
+        self,
+        verts: Tensor,
+        faces: Tensor,
+        k_evecs: int=10,
         device: torch.device = torch.device('cpu'),
     ) -> None:
         super(LBOEmbedding, self).__init__()
 
         # Eliminate unused vertices and offset faces
-        self.verts, self.faces = self.remove_detached_verts(verts, faces)
+        self.verts, self.faces = remove_detached_verts(verts, faces)
 
         # Compute symmetric normalized graph Laplacian
-        self.A: Tensor = self.adjacency_matrix(self.verts, self.faces[:, 1:]).to(device)
-        self.D: Tensor = self.degree_matrix(self.A).to(device)
-        self.L: Tensor = self.sym_norm_graph_laplacian(self.D, self.A)
+        A: Tensor = self.adjacency_matrix(self.verts, self.faces[:, 1:])
+        D: Tensor = self.degree_matrix(A)
+        L: Tensor = self.sym_norm_graph_laplacian(D.to(device), A.to(device))
 
         # Compute the first k eigenpairs in ascending order
-        self.evals, self.evecs = torch.lobpcg(self.L, k=k_evecs, largest=False, method='ortho')
-        self.evecs = torch.nn.functional.normalize(self.evecs, p=2, dim=0)
-
-        # Move data back to CPU to avoid OOM
-        self.A = self.A.cpu()
-        self.D = self.D.cpu()
-        self.L = self.L.cpu()
-        self.evals: Tensor = self.evals.cpu()
-        self.evecs: Tensor = self.evecs.cpu()
+        _, self.evecs = torch.lobpcg(L, k=k_evecs, largest=False, method='ortho')
+        self.evecs = torch.nn.functional.normalize(self.evecs, p=2, dim=0).cpu()
 
     def __call__(self, verts: Tensor, bary: Tensor | None = None) -> Tensor:
         bary = torch.full((verts.size(0), 3), 1/3) if bary is None else bary
+        verts = einops.repeat(verts, 'V -> V C', C=3) if verts.ndim == 1 else verts
         assert verts.size(0) == bary.size(0), 'size mismatch between verts and barycentric values'
         assert torch.all(einops.reduce(bary, 'B C -> B', 'sum') - 1 < 1e-6), 'barycentric values do not sum to 1'
 
         # Select values from each eigenvector for each given vertex (and the neighbors)
-        index: Tensor = einops.repeat(verts, 'B C -> B C K', K=self.evecs.size(1))
+        index: Tensor = einops.repeat(verts, 'B C -> C B K', K=self.evecs.size(1))
         evecs: Tensor = einops.repeat(self.evecs, 'V K -> C V K', C=verts.size(1))
-        print(evecs.shape, index.shape)
-        eigen: Tensor = torch.gather(input=evecs, dim=-2, index=index)
+        eigen: Tensor = torch.gather(input=evecs, dim=1, index=index)
+        eigen = einops.rearrange(eigen, 'C V K -> V C K')
 
         # Compute baricentric interpolation using eigenvector values
         eigen = torch.einsum('VCK,VC->VK', eigen, bary)
 
         # Use sqrt(N) as factor
-        coef: Tensor = torch.sqrt(torch.tensor(self.verts.size(0)))
-        eigen = coef * eigen
+        coefs: Tensor = torch.sqrt(torch.tensor(self.verts.size(0)))
+        eigen = coefs * eigen
         return eigen
 
     @staticmethod
@@ -85,20 +125,20 @@ class LBOEmbedding:
         faces: Tensor = torch.tensor(poly.faces.reshape((-1, 4)))
         return LBOEmbedding(verts, faces, *args, **kwargs)
 
-    @staticmethod
-    def remove_detached_verts(verts: Tensor, faces: Tensor) -> t.Tuple[Tensor, Tensor]:
-        # Eliminate vertices that don't appear in faces
-        verts_idx: Tensor = torch.arange(verts.size(0))
-        verts_idx_inuse: Tensor = torch.unique(faces.flatten())
-        verts_idx_inuse: Tensor = torch.isin(verts_idx, verts_idx_inuse)
-        verts_inuse: Tensor = verts[verts_idx_inuse, :]
 
-        # Offset vertex indices in each face to account for removal
-        offsets: Tensor = (~verts_idx_inuse).cumsum(0)
-        offset = np.vectorize(lambda x: x - offsets[x])
-        faces_inuse = faces.numpy()
-        faces_inuse[:, 1:] = np.apply_along_axis(offset, axis=0, arr=faces_inuse[:, 1:])
-        faces_inuse = torch.from_numpy(faces_inuse)
+def remove_detached_verts(verts: Tensor, faces: Tensor) -> t.Tuple[Tensor, Tensor]:
+    # Eliminate vertices that don't appear in faces
+    verts_idx: Tensor = torch.arange(verts.size(0))
+    verts_idx_inuse: Tensor = torch.unique(faces.flatten())
+    verts_idx_inuse: Tensor = torch.isin(verts_idx, verts_idx_inuse)
+    verts_inuse: Tensor = verts[verts_idx_inuse, :]
 
-        # Give back data in the same order
-        return verts_inuse, faces_inuse
+    # Offset vertex indices in each face to account for removal
+    offsets: Tensor = (~verts_idx_inuse).cumsum(0)
+    offset = np.vectorize(lambda x: x - offsets[x])
+    faces_inuse = faces.numpy()
+    faces_inuse[:, 1:] = np.apply_along_axis(offset, axis=0, arr=faces_inuse[:, 1:])
+    faces_inuse = torch.from_numpy(faces_inuse)
+
+    # Give back data in the same order
+    return verts_inuse, faces_inuse
