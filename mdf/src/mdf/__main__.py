dd82eebade6d60f2d5a42a1caf3f3cf7ac890ee7
Andrei Ionescu <44782217+fusedbloxxer@users.noreply.github.com>
2024-05-19 13:45:27 +0300

    Delete mdf/src/mdf/__main__.py

--- mdf/src/mdf/__main__.py
+++
@@ -1,8 +0,0 @@
-from . import *
-from .data import DataManager
-
-# Download standard shapes
-data_manager = DataManager(download=True, data_dir=data_dir, cache_dir=cache_dir)
-
-# Plot shapes
-print(data_manager.objects.woody.content.vertices)
